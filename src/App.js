2367f31e45bacd0dab6b278d6a8836ac94ad362e
Sanya Moses Adeniyi <54499732+akpcode@users.noreply.github.com>
2024-04-19 21:23:42 +0100

    creating form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import List from './List/List';
 import Navbar from "./navigation/Navbar";
+import Form from './form';
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <List/>
+     
+    { /*<Navbar/>
+     <List/>*/}
+     <Form/>
     </div>
   );
 }
---
+++ src/form/index.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import "./form.css";
+
+function Form() {
+    const [name, setName] = useState("");
+    const [Password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+  console.log(name, Password, email);
+  
+    function handleSubmit(e){
+    e.preventDefault();
+    
+  }
+    return (
+    <form className='form_container' onSubmit={handleSubmit}>
+        <div className='form_body'>
+        <label>Name</label>
+        <input type="text"
+         className="input_color" 
+        value={name}
+         onChange={(e)=> {
+        setName(e.target.value);
+        }} />
+        </div>
+        <div className='form_body'>
+        <label>Password</label>
+        <input type="Password"
+         className="input_color" 
+         value={Password}
+         onChange={(e)=> {
+        setPassword(e.target.value);
+    }}/>
+        </div>
+        <div className='form_body'>
+        <label>Email</label>
+        <input type="Email"
+         className="input_color" 
+         value={email}
+         onChange={(e)=> {
+        setEmail(e.target.value);
+    }} />
+        </div>
+        <input type="submit" name="submit" className="form_btn"/>
+    </form>
+  )
+}
+
+export default Form;
